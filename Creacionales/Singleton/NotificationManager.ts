8678e53226e32ec7714d24fb3802dc85f8a46d21
RRosas97 <ricardo.rosas@axity.com>
2024-08-20 22:52:38 +0000

    Cambios en los archivos de Singleton

--- Creacionales/Singleton/NotificationManager.ts
+++ Creacionales/Singleton/NotificationManager.ts
@@ -6,10 +6,17 @@ import { SMSNotificationService } from './SMSNotificationService';
 export class NotificationManager {
     private emailService: INotificationService;
     private smsService: INotificationService;
-
-    constructor() {
+    private static instanciaUnica:NotificationManager;
+//Se pone un private al constructor para que se 
+    private constructor() {
         this.emailService = new EmailNotificationService();
         this.smsService = new SMSNotificationService();
+    }
+    public static getSingleInstance():NotificationManager{
+       if(!NotificationManager.instanciaUnica){
+        NotificationManager.instanciaUnica = new NotificationManager() //Ahora sí se hace la instancia única, si no existe desde antes
+       }
+       return NotificationManager.instanciaUnica;
     }
 
     public notifyByEmail(message: string, recipient: string): void {
--- Creacionales/Singleton/index.ts
+++ Creacionales/Singleton/index.ts
@@ -3,3 +3,10 @@ import { EnrollmentService } from './EnrollmentService';
 import { BillingService } from './BillingService';
 
 
+const enrollmentService = new EnrollmentService();
+const billingService = new BillingService();
+
+const contactMail:string = 'ricardo.rosas@axity.com';
+
+enrollmentService.enrollStudent(contactMail);
+billingService.notifyPaymentDue(contactMail);
